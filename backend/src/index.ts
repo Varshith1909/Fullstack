ed33a485ddb29c5b3c84ee174094a2da608ad606
varshith1909 <vc8@pdx.edu>
2023-06-04 08:49:32 -0400

    Resolved a port error

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,13 +3,13 @@ import app from "./app.js";
 
 dotenv.config();
 
+const port = process.env.BACKEND_PORT || 8082;
+const host = process.env.HOST || '0.0.0.0';
 
-app.listen({port:Number(process.env.PORT), host:process.env.host},
-  (err,address) => {
-  	if(err){
-		console.error(err);
-		process.exit(1);
-	}
-	  console.log(`Started server at ${address}`);
+app.listen({ port: Number(port), host }, (err, address) => {
+  if (err) {
+	console.error(err);
+	process.exit(1);
   }
-  );
+  console.log(`Started server at ${address}`);
+});
