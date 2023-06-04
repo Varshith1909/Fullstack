5210e7ef72c4562861c8cdced1f2af4050f21185
Harsha0526 <hgattu@pdx.edu>
2023-06-04 13:06:43 -0400

    Completed the migrations

---
+++ backend/src/db/migrations/Migration20230604164859_Suppliers.ts
@@ -0,0 +1,33 @@
+import { Migration } from '@mikro-orm/migrations';
+
+export class Migration20230604164859_Suppliers extends Migration {
+
+  async up(): Promise<void> {
+    this.addSql('alter table "categories" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "products" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "sales" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "suppliers" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "purchases" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "user" alter column "id" type int using ("id"::int);');
+  }
+
+  async down(): Promise<void> {
+    this.addSql('alter table "categories" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "products" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "sales" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "suppliers" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "purchases" alter column "id" type int using ("id"::int);');
+
+    this.addSql('alter table "user" alter column "id" type int using ("id"::int);');
+  }
+
+}
