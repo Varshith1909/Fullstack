b6d0eb5a3035b564687f2ec582d065a93c2eabe3
Harsha0526 <hgattu@pdx.edu>
2023-06-06 00:20:05 -0400

    added test cases for frontend

--- frontend/test/App.test.tsx
+++ frontend/test/App.test.tsx
@@ -1,8 +1,18 @@
-import { screen } from '@testing-library/react';
+// import { screen } from '@testing-library/react';
 import { expect, test } from 'vitest';
+import {render, fireEvent, screen} from '@testing-library/react'
+import App from "../src/App.js"
 
 test("Math.sqrt()", () => {
   expect(Math.sqrt(4)).toBe(2);
   expect(Math.sqrt(144)).toBe(12);
   expect(Math.sqrt(2)).toBe(Math.SQRT2);
 });
+
+describe("Renders React components correctly" , async()=>{
+  let testrender =render(<App />)
+  const h1 =await screen.queryByText("vite + React")
+  
+  expect(h1).not.toBeNull();
+  expect(h1).toBeVisible();
+})
