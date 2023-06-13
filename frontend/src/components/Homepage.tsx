293b392bf1fa1b925649fc443da0f2eff2e55a20
varshith1909 <vc8@pdx.edu>
2023-06-13 17:42:50 -0400

    Added Minio to docker compose

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -3,8 +3,8 @@ import React from 'react';
 export const Header = () => {
   return (
     <div>
-      <h1>Doggr</h1>
-      <h3>Where your pets find love(tm)</h3>
+      <h1>Pharma</h1>
+      <h3>Where your health is taken care</h3>
       <br />
     </div>
   );
