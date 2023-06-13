9470c7ac586a89b7b0c89206b5ec4ee81d1a6ec2
Harsha0526 <hgattu@pdx.edu>
2023-06-13 03:10:41 -0400

    Solved auth0 error

--- frontend/.env.ts
+++ frontend/.env.ts
@@ -1,4 +1,4 @@
 export const API_HOST = "localhost";
 export const PORT = 8081;
 export const REACT_APP_AUTH0_DOMAIN = "dev-2ere3xom3pqjdylj.us.auth0.com";
-export const REACT_APP_AUTH0_CLIENT_ID = "ehO7yfRcv3iDEShvOINAKBSLToI15fWE";
+export const REACT_APP_AUTH0_CLIENT_ID = "jVB18Gzldk9aNyJbfBIxgP2X3CExFEIy";
