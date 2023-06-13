7cc342f5d8a2cf7d098bef8b98cb98f423b54745
Harsha0526 <hgattu@pdx.edu>
2023-06-13 03:38:30 -0400

    modified the login function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,40 +1,39 @@
-import LoginButton from "@/loginButton.tsx";
-import LogoutButton from "@/logoutButton.tsx";
-import { useState } from 'react'
-import reactLogo from '@images/react.svg'
-import viteLogo from '/vite.svg'
-import "@css/App.css";
-import {Header} from "@/components/Homepage.tsx";
-import { Link, Route, Routes, Router, BrowserRouter } from "react-router-dom";
-import {ProductList} from "@/components/ProductList.tsx";
-import AddProduct  from '@/components/PurchaseList.tsx';
-import loginButton  from "@/loginButton.tsx";
-import logoutButton from "@/logoutButton.tsx";
+import React from 'react';
+import { useAuth0 } from '@auth0/auth0-react';
+import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
+import { Header } from '@/components/Homepage.tsx';
+import { ProductList } from '@/components/ProductList.tsx';
+import AddProduct from '@/components/PurchaseList.tsx';
+import LoginButton from '@/loginButton.tsx';
+import LogoutButton from '@/logoutButton.tsx';
 
 export function App() {
-return(
+  const { isAuthenticated } = useAuth0();
   
-  <BrowserRouter>
-  <div className="App">
-    <nav>
-      <div className={"menu"}>
-        <Link to="/">Home</Link> ||
-        <Link to="/products"> Search</Link> ||
-        <Link to="/api/product"> Purchases</Link>
+  return (
+    <BrowserRouter>
+      <div className="App">
+       
+        {isAuthenticated && (
+          <>
+            <nav>
+              <div className="menu">
+                <Link to="/">Home</Link> || <Link to="/products">Search</Link> ||{' '}
+                <Link to="/api/product">Purchases</Link>
+              </div>
+            </nav>
+            <Routes>
+              <Route path="/" element={<Header />} />
+              <Route path="/products" element={<ProductList />} />
+              <Route path="/api/product" element={<AddProduct />} />
+            </Routes>
+            <LogoutButton />
+          </>
+        )}
+        {!isAuthenticated && <LoginButton />}
       </div>
-    </nav>
-    <Routes>
-      <Route path="/" element={<Header />} />
-      <Route path='/products' element={<ProductList />} />
-      <Route path="/api/product" element={<AddProduct />} />
-    </Routes>
-    <LoginButton />
-    <LogoutButton />
-    
-  </div>
-  </BrowserRouter>
-)
-
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
