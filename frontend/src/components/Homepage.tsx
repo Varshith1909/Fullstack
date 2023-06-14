828447e478c92b624fafcf25fd9aa9a0600f8519
varshith1909 <vc8@pdx.edu>
2023-06-14 14:29:46 -0400

    Added image to the Hompepage

--- frontend/src/components/Homepage.tsx
+++ frontend/src/components/Homepage.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import '../assets/css/Homepage.css';
+import Img1 from '@/assets/images/pharma.jpeg';
+
 export const Header = () => {
   return (
     <div className="header-container">
-      <h3>We care fro your health Every moment</h3>
-     
-      <br/>
-      <p>If you are searching for the best website to assist you in managing all the medicines in your store, you have found the right place.</p>
-    
+      <div className="image-container">
+        <img src={Img1} alt="My Image" />
+      </div>
+      <div className="text-container">
+        <h3>We care for your health Every moment</h3>
+        <p>If you are searching for the best website to assist you in managing all the medicines in your store, you have found the right place.</p>
+      </div>
     </div>
   );
 };
