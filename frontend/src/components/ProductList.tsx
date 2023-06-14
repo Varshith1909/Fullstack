02756e6a8f906e839a7647b42bff321fd3a17517
varshith1909 <vc8@pdx.edu>
2023-06-14 11:55:02 -0400

    Tried to add aan image to products

--- frontend/src/components/ProductList.tsx
+++ frontend/src/components/ProductList.tsx
@@ -1,6 +1,6 @@
 import LoginButton from "@/loginButton.tsx";
 import LogoutButton from "@/logoutButton.tsx";
-import {useAuth0} from "@auth0/auth0-react";
+import { useAuth0 } from "@auth0/auth0-react";
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 
@@ -9,68 +9,62 @@ export const ProductList = () => {
     const [searchQuery, setSearchQuery] = useState("");
     const [filteredUsers, setFilteredUsers] = useState([]);
     const { isAuthenticated } = useAuth0();
-
+    
     const fetchProducts = async () => {
         const productRes = await axios.get("http://localhost:8082/products");
         return productRes.data;
     };
-
+    
     useEffect(() => {
         fetchProducts().then((data) => {
             setUsers(data);
             setFilteredUsers(data);
         });
     }, []);
-
+    
     const handleSearch = () => {
         if (searchQuery.trim() !== "") {
             const filtered = users.filter((user) =>
-                user.name.toLowerCase().includes(searchQuery.toLowerCase())
+              user.name.toLowerCase().includes(searchQuery.toLowerCase())
             );
             setFilteredUsers(filtered);
         } else {
             setFilteredUsers(users);
         }
     };
-
+    
     return (
-        <div>
-            {isAuthenticated && (
-              <>
-                  
-                  <h2>Products</h2>
-                  <div>
-                      <input
-                        type="text"
-                        value={searchQuery}
-                        onChange={(e) => setSearchQuery(e.target.value)}
-                      />
-                      <button onClick={handleSearch}>Search</button>
-                  </div>
-                  {filteredUsers.length > 0 ? (
-                    <ul>
-                        {filteredUsers.map((user) => (
-                          <li key={user.name}>
-                              {user.name} - {user.price - user.discount} - {user.description}
-                          </li>
-                        ))}
-                    </ul>
-                  ) : (
-                    <p>No products found.</p>
-                  )}
-              </>
-            )}
-            {!isAuthenticated &&
-            
+      <div>
+          {isAuthenticated && (
+            <>
+                <h2>Products</h2>
+                <div>
+                    <input
+                      type="text"
+                      value={searchQuery}
+                      onChange={(e) => setSearchQuery(e.target.value)}
+                    />
+                    <button onClick={handleSearch}>Search</button>
+                </div>
+                {filteredUsers.length > 0 ? (
+                  <ul>
+                      {filteredUsers.map((user) => (
+                        <li key={user.name}>
+                            {user.name} - {user.price - user.discount} - {user.description}
+                            <img src={user.imageUri} alt="Product" />
+                        </li>
+                      ))}
+                  </ul>
+                ) : (
+                  <p>No products found.</p>
+                )}
+            </>
+          )}
+          {!isAuthenticated && (
             <div>
-                
-                <h1> Login to see</h1>
+                <h1>Login to see</h1>
             </div>
-            
-            }
-            
-            
-        </div>
-        
+          )}
+      </div>
     );
 };
